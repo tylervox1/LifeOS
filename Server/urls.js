6b6224ac3bf5e762726d47a283956891f89d43ee
tylervox1 <tylervox1@gmail.com>
2026-09-13 04:38:42 -0700

    Delete Server directory

--- Server/urls.js
+++
@@ -1,4 +0,0 @@
-export function appPublicUrl(){
-  const url = process.env.APP_PUBLIC_URL || process.env.APP_BASE_URL || process.env.RENDER_EXTERNAL_URL || '';
-  return String(url).replace(/\/$/, '');
-}
